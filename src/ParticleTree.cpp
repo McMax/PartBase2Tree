ca326fc525e094eaaab56303ad1ae2f97b0a2596
McMax <wwy_l935@wp.pl>
2013-03-06 12:53:55 +0100

    Merged ParticleTree class with Mixer

--- src/ParticleTree.cpp
+++ src/ParticleTree.cpp
@@ -10,9 +10,19 @@ ParticleTree::ParticleTree(TString filename)
 	part_id = ev_id = 0;
 }
 
+void ParticleTree::SetEvid(UInt_t eid)
+{
+	ev_id = eid;
+}
+
 void ParticleTree::BeginEvent()
 {
 	event->SetEid(++ev_id);
+}
+
+void ParticleTree::BeginEvent(UInt_t eid)
+{
+	event->SetEid(eid);
 }
 
 UInt_t ParticleTree::Check()
@@ -28,9 +38,19 @@ void ParticleTree::EndEvent()
 	event->Clear();
 }
 
+void ParticleTree::CancelEvent()
+{
+	event->Clear();
+}
+
 void ParticleTree::AddParticle(Short_t charge, Float_t bx, Float_t by,  Float_t px, Float_t py, Float_t pz, Float_t dedx, Float_t dedx_vtpc1, Float_t dedx_vtpc2, Float_t dedx_mtpc, Int_t ndedx, Int_t ndedx_vtpc1, Int_t ndedx_vtpc2, Int_t ndedx_mtpc)
 {
 	event->AddParticle(++part_id, charge, bx, by, px, py, pz, dedx, dedx_vtpc1, dedx_vtpc2, dedx_mtpc, ndedx, ndedx_vtpc1, ndedx_vtpc2, ndedx_mtpc);
+}
+
+void ParticleTree::AddParticle(UInt_t pid, Short_t charge, Float_t bx, Float_t by,  Float_t px, Float_t py, Float_t pz, Float_t dedx, Float_t dedx_vtpc1, Float_t dedx_vtpc2, Float_t dedx_mtpc, Int_t ndedx, Int_t ndedx_vtpc1, Int_t ndedx_vtpc2, Int_t ndedx_mtpc)
+{
+	event->AddParticle(pid, charge, bx, by, px, py, pz, dedx, dedx_vtpc1, dedx_vtpc2, dedx_mtpc, ndedx, ndedx_vtpc1, ndedx_vtpc2, ndedx_mtpc);
 }
 
 void ParticleTree::Close()
